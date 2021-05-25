b5fb02e1382650da21c2012f85d8e2678cb986cc
neuroplane <aleksey@boyarkin.com>
2021-05-25 18:56:47 +0300

    Init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div v-if="$storage.state.user" class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container v-if="$storage.state.user" >
+    <v-row justify="center">
+      <v-date-picker
+          v-model="picker"
+          type="month"
+      ></v-date-picker>
+    </v-row>
+  </v-container>
 </template>
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      picker: new Date().toISOString().substr(0, 10),
+    }
+  }
+}
+</script>
