cde3a8057c18e3c97314cc6200e5f8091dfb6d6e
neuroplane <aleksey@boyarkin.com>
2021-06-07 16:42:06 +0300

    Commit

--- SQL/__huinia.sql
+++ SQL/__huinia.sql
@@ -1,20 +1,24 @@
 --Выдрать месяц из даты
 select * from users u where (select date_part('month',u.birthday) = (select date_part('month',(select last_day(now())))));
 
+select u.surname from users u
+    join job_status js on u.id = js.user_id
+where u.birthday
+    BETWEEN
+    '1970-12-31'
+    AND
+    '1979-12-31';
+
 --Текущая дата
 select now();
 
-create or replace funCtion last_day(fromdt anyelement)
-    returns date as
-$BODY$
-SELECT (date_trunc('MONTH', cast(fromdt as date)) + INTERVAL '1 MONTH - 1 day')::date;
-$BODY$
-    LANGUAGE sql VOLATILE
-                 COST 100;
-ALTER FUNCTION last_day(anyelement)
-    OWNER TO neuroplane;
+--------
+SELECT TO_DATE('2021' || (select to_char(now(),'mm')) || '01','YYYYMMDD');
 
-select last_day(now());
-
-
-select date_part('month',(select last_day(now())))
+SELECT u.id,
+       ((SELECT u.surname) || ' ' || (SELECT LEFT(u.name,1) || '.') || (SELECT LEFT(u.patronymic,1)) || '.') as fio
+from  users u
+          join job_status js on u.id = js.user_id
+where js.end_date is null
+or '2021-05-01' > js.start_date and '2021-05-01' < js.end_date
+order by u.surname;
