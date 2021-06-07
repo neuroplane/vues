1886c1dd84ef96a5054b8868e32fd759f586480f
neuroplane <aleksey@boyarkin.com>
2021-06-07 14:41:45 +0300

    Commit

--- SQL/last_day.sql
+++ SQL/last_day.sql
@@ -1,9 +1,3 @@
---Выдрать месяц из даты
-select * from users u where (select date_part('month',u.birthday) = (select date_part('month',(select last_day(now())))));
-
---Текущая дата
-select now();
-
 create or replace funCtion last_day(fromdt anyelement)
     returns date as
 $BODY$
@@ -15,6 +9,3 @@ ALTER FUNCTION last_day(anyelement)
     OWNER TO neuroplane;
 
 select last_day(now());
-
-
-select date_part('month',(select last_day(now())))
