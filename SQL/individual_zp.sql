969e42be461c690cf54695a9876b7a14379292d7
neuroplane <aleksey@boyarkin.com>
2021-06-19 16:37:29 +0300

    Commit

--- SQL/individual_zp.sql
+++ SQL/individual_zp.sql
@@ -8,8 +8,23 @@ DECLARE
     _report_date date;
     _user_id bigint;
     _selected_user int;
+    _month int;
+    _year int;
+    _ktu_month_sum float;
+    _ktu_month_amount float;
+    _ktu_month_lines float;
+    _ktu_month_documents float;
+    _actual_ktu float;
 BEGIN
+
     _selected_user = requireint(params, 'selected_user');
+    _month = requireint(params, 'month');
+    _year = requireint(params, 'year');
+    _ktu_month_sum = (SELECT SUM(c_sum) from public.ktu k where k.period_month = _month and k.period_year = _year);
+    _ktu_month_amount = (SELECT SUM(c_amount) from public.ktu k where k.period_month = _month and k.period_year = _year);
+    _ktu_month_lines = (SELECT SUM(c_amount) from public.ktu k where k.period_month = _month and k.period_year = _year);
+    _ktu_month_documents = (SELECT SUM(c_documents) from public.ktu k where k.period_month = _month and k.period_year = _year);
+    select k.c_sum + k.c_documents from ktu k where k.user_id = _selected_user into _actual_ktu;
     _report_date = requiredate(params, 'report_date');
     _user_id = get_user(_token);
     IF _user_id IS NULL THEN RAISE EXCEPTION 'Wrong username and/or password' USING ERRCODE='ER403'; END IF;
@@ -17,7 +32,7 @@ BEGIN
     SELECT json_agg(a)
     INTO _response
     FROM (
-        select u.id, u.surname, t.ndfl, fb.fine, fb.bonus, c.amount, rh.role, wh.hours from users u
+        select _actual_ktu as ktu, u.id, u.surname, t.ndfl, fb.fine, fb.bonus, c.amount, rh.role, wh.hours from users u
         left join taxes t on u.id = t.user_id
         left join fines_bonuses fb on t.user_id = fb.user_id
         left join credit c on u.id = c.user_id
