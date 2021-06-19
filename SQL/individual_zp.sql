0aee079389fd127f292111f6ef8f1da5c423a842
neuroplane <aleksey@boyarkin.com>
2021-06-19 17:52:45 +0300

    Commit

--- SQL/individual_zp.sql
+++ SQL/individual_zp.sql
@@ -15,6 +15,7 @@ DECLARE
     _ktu_month_lines float;
     _ktu_month_documents float;
     _actual_ktu float;
+    _ndfl float;
 BEGIN
 
     _selected_user = requireint(params, 'selected_user');
@@ -24,21 +25,22 @@ BEGIN
     _ktu_month_amount = (SELECT SUM(c_amount) from public.ktu k where k.period_month = _month and k.period_year = _year);
     _ktu_month_lines = (SELECT SUM(c_amount) from public.ktu k where k.period_month = _month and k.period_year = _year);
     _ktu_month_documents = (SELECT SUM(c_documents) from public.ktu k where k.period_month = _month and k.period_year = _year);
-    select k.c_sum + k.c_documents from ktu k where k.user_id = _selected_user into _actual_ktu;
+
+    select k.c_sum/_ktu_month_sum + k.c_documents/_ktu_month_documents
+    from ktu k
+    where k.user_id = _selected_user and k.period_year = _year and k.period_month = _month
+    into _actual_ktu;
+
+    SELECT ndfl from taxes where user_id = _selected_user and period_year = _year and period_month = _month into _ndfl;
     _report_date = requiredate(params, 'report_date');
     _user_id = get_user(_token);
-    IF _user_id IS NULL THEN RAISE EXCEPTION 'Wrong username and/or password' USING ERRCODE='ER403'; END IF;
     --------------------------------------------------------------------------------------------------------
     SELECT json_agg(a)
     INTO _response
     FROM (
-        select _actual_ktu as ktu, u.id, u.surname, t.ndfl, fb.fine, fb.bonus, c.amount, rh.role, wh.hours from users u
-        left join taxes t on u.id = t.user_id
-        left join fines_bonuses fb on t.user_id = fb.user_id
-        left join credit c on u.id = c.user_id
-        left join role_history rh on u.id = rh.user_id
-        left join work_hours wh on u.id = wh.user_id
-        where u.id = _selected_user
+        select round((_actual_ktu*100)::NUMERIC,2) as ktu, _ndfl, u.id, u.surname from users u
+        full join ktu k2 on u.id = k2.user_id
+        where u.id = _selected_user and k2.period_month = _month and k2.period_year = _year
     ) AS a;
     --------------------------------------------------------------------------------------------------------
     RETURN coalesce(_response,'[]');
@@ -47,4 +49,4 @@ $$;
 
 alter function individualzp(json, uuid) owner to neuroplane;
 
-select public.individualzp('{"report_date":"2021-05-01", "selected_user":29}'::json, '11609376-ff57-401e-88a4-53f4c0904fdb'::uuid);
+select public.individualzp('{"report_date":"2021-05-01", "selected_user":19, "month": 4, "year": 2021}'::json, '11609376-ff57-401e-88a4-53f4c0904fdb'::uuid);
