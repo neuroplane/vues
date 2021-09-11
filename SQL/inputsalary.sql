f3426daf00e9ad446e775c6f91812a22ac97ee2d
alekseyboyarkin <aleksey@boyarkin.com>
2021-09-11 18:52:09 +0300

    MBAIR commit

--- SQL/inputsalary.sql
+++ SQL/inputsalary.sql
@@ -3,8 +3,8 @@ create or replace function inputsalary(params json, token uuid) returns json
 as
 $$
     for (item of params.items){
-         plv8.execute('insert into salary(user_id, amount, period_month, period_year) values($1, $2, $3, $4)',
-        [item.user_id, item.amount != "" ? item.amount : null, item.period_month, item.period_year]);
+         plv8.execute('insert into salary(user_id, amount, period_month, period_year, role) values($1, $2, $3, $4, $5)',
+        [item.user_id, item.amount != "" ? item.amount : null, item.period_month, item.period_year, item.role]);
     }
     return {inserted: params.items}
 
